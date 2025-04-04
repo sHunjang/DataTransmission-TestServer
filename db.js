9a94834491a200a668b88d973c37c149a54b817b
SH <seunghunjang@SH-4.local>
2025-04-04 12:27:07 +0900

    feat: .env File

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 
+require("dotenv").config();
+
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "DataTransmission",
-    password: "1234",
-    port: 5432,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
 });
 
 module.exports = pool;
